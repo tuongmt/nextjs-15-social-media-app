49bccb9bff8c1bbe2b31a57bdb13db17b8810319
Ma Tuan Tuong <130485925+tuongmt@users.noreply.github.com>
2024-08-27 21:42:39 +0700

    Drag & drop and copy-paste file uploads

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -10,10 +10,11 @@ import "./styles.css";
 import { useSubmitPostMutation } from "./mutations";
 import LoadingButton from "@/components/LoadingButton";
 import useMediaUpload, { Attachment } from "./useMediaUpload";
-import { useRef } from "react";
+import { ClipboardEvent, useRef } from "react";
 import { ImageIcon, Loader2, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useDropzone } from "@uploadthing/react";
 
 export default function PostEditor() {
   const { user } = useSession();
@@ -28,6 +29,12 @@ export default function PostEditor() {
     removeAttachment,
     reset: resetMediaUploads,
   } = useMediaUpload();
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: startUpload,
+  });
+
+  const { onClick, ...rootProps } = getRootProps();
 
   const editor = useEditor({
     extensions: [
@@ -61,14 +68,25 @@ export default function PostEditor() {
     );
   }
 
+  function onPaste(e: ClipboardEvent<HTMLInputElement>){
+    const files = Array.from(e.clipboardData.items)
+    .filter(item=>item.kind=== "file")
+    .map(item => item.getAsFile()) as File[];
+    startUpload(files);
+  }
+
   return (
     <div className="flex flex-col gap-5 rounded-2xl border bg-card p-5 shadow-sm">
       <div className="flex gap-5">
         <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
-        <EditorContent
-          editor={editor}
-          className="max-h-[20rem] w-full overflow-y-auto rounded-2xl border bg-background px-5 py-3"
-        />
+        <div {...rootProps} className="w-full">
+          <EditorContent
+            editor={editor}
+            className={cn("max-h-[20rem] w-full overflow-y-auto rounded-2xl border bg-background px-5 py-3", isDragActive && "outline-dashed")}
+            onPaste={onPaste}
+          />
+          <input {...getInputProps()} onPaste={onPaste}/>
+        </div>
       </div>
       {/* !! return boolean type, false (0, null, undefined) otherwise true */}
       {!!attachments.length && (
