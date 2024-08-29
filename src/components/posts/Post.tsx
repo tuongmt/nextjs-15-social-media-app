7c4a6e0669bdb2c09a2ca7e2d9f0eb11fba881c9
Ma Tuan Tuong <130485925+tuongmt@users.noreply.github.com>
2024-08-29 13:52:08 +0700

    Post details page

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -41,6 +41,7 @@ export default function Post({ post }: PostProps) {
             <Link
               href={`/posts/${post.id}`}
               className="block text-sm text-muted-foreground hover:underline"
+              suppressHydrationWarning
             >
               {formatRelativeDate(post.createAt)}
             </Link>
