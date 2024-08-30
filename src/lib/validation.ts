4cea18d1e6d062a123afae31a11e5688c3a948a6
Ma Tuan Tuong <130485925+tuongmt@users.noreply.github.com>
2024-08-30 17:49:35 +0700

    Comments feature (with infinite loading)

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -22,7 +22,7 @@ export type LogInValues = z.infer<typeof loginSchema>;
 
 export const createPostSchema = z.object({
   content: requiredString,
-  mediaIds: z.array(z.string()).max(5,"Cannot have more than 5 attachments")
+  mediaIds: z.array(z.string()).max(5, "Cannot have more than 5 attachments"),
 });
 
 export const updateUserProfileSchema = z.object({
@@ -31,3 +31,7 @@ export const updateUserProfileSchema = z.object({
 });
 
 export type UpdateUserProfileValues = z.infer<typeof updateUserProfileSchema>;
+
+export const createCommentSchema = z.object({
+  content: requiredString,
+});
